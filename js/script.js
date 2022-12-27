0e9dd13914164720da1135748f8e8a447a03fd11
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2022-12-27 20:55:32 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+navbar = document.querySelector('.header .flex .navbar');
+document.querySelector('#menu-btn').onclick = () => {
+   navbar.classList.toggle('active');
+   profile.classList.remove('active');
+}
+profile = document.querySelector('.header .flex .profile');
+document.querySelector('#user-btn').onclick = () => {
+   profile.classList.toggle('active');
+   navbar.classList.remove('active');
+}
+window.onscroll = () => {
+   navbar.classList.remove('active');
+   profile.classList.remove('active');
+}
+function loader(){
+   document.querySelector('.loader').style.display = 'none';
+}
+//aviso 1000 = 1 seconds
+//então 2000 = 2 seconds
+function fadeOut(){
+   setInterval(loader,2000);
+}
+window.onload = fadeOut();
